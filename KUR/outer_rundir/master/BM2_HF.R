c1ec691a479ff383b2db8e6be9ce1e65d036af71
rqmacasieb <reygie.macasieb@research.uwa.edu.au>
2024-07-30 18:42:10 +0800

    KUR files added

---
+++ KUR/outer_rundir/master/BM2_HF.R
@@ -0,0 +1,24 @@
+dv <- read.csv("dv.dat")
+
+x1 <- dv[1,1]
+x2 <- dv[2,1]
+x3 <- dv[3,1]
+
+obj1<- 0
+for (i in 1:2){
+  obj <- -10*exp(-0.2*sqrt(dv[i,1]^2+dv[i+1,1]^2))
+  obj1 <- obj1 + obj
+}
+
+obj2<- 0
+for (i in 1:3){
+  obj <- abs(dv[i,1])^0.8 + 5*sin((dv[i,1]^3))
+  obj2 <- obj2 + obj
+}
+
+obj1sd <- 0
+obj2sd <- 0
+Pf_obj_2 <- 1
+
+output <- c(obj1,obj2, obj1sd, obj2sd, Pf_obj_2)
+write.csv(output, "hf_output.dat", quote = F, row.names = F)
